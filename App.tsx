9f781079717965433652e5c27472aa49ed664461
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-10-20 21:54:19 -0300

    tipando styled components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Dashboard />
+    </ThemeProvider>
   );
 }
