011f061fdbd82996d853eba78338da1b344128bf
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-10-20 23:27:54 -0300

    header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { Container, Title } from './styles'
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+
+} from './styles'
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/72214347?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Bruno</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
