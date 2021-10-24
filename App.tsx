2ac4f7b6bc3615661d82e880fdeb383b34010a84
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-10-23 21:27:34 -0300

    category screen

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
